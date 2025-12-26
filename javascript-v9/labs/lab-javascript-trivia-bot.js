e3d7928205aaf14fa86c7c634e26fd3e52d772aa
AlexeiLuchian <alexeiluchian999@gmail.com>
2025-12-26 16:58:20 +0200

    Add JavaScript Trivia Bot Lab

---
+++ javascript-v9/labs/lab-javascript-trivia-bot.js
@@ -0,0 +1,15 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "techie", botLocation = "botaeneris", favoriteLanguage = "botanica";
+
+console.log("My name is " + botName +" and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "Did you know that the " + favoriteLanguage + " language doesn't exist?";
+console.log(codingFact);
+codingFact = "There's actually only one person speaking " + favoriteLanguage + " in the whole universe!";
+console.log(codingFact);
+codingFact = favoriteLanguage + " language is so hard, duolingo didn't even add it throughout their options!";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
