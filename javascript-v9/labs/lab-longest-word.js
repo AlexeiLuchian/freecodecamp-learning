9179fcd86da6ebfb54b816e6c81706e6a6080831
AlexeiLuchian <alexeiluchian999@gmail.com>
2025-12-31 12:07:03 +0200

    Add Factorial calc and Longest word Labs

---
+++ javascript-v9/labs/lab-longest-word.js
@@ -0,0 +1,11 @@
+function findLongestWordLength(str) {
+  const words = str.split(' ');
+  let maxLength = 0;
+  
+  for (const word of words) {
+    if (word.length > maxLength) {
+      maxLength = word.length;
+    }
+  }
+  return maxLength;
+}
